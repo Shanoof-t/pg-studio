7a556dade0699969f2ebc1f2f65a33225bbfc976
Shanoof <shanoof.001@gmail.com>
2025-08-16 12:11:51 +0530

    fix:.env path in index

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ const { spawn } = require("child_process");
 
 const originalCwd = process.env.INIT_CWD || process.cwd();
 
-console.log("process.env.PWD : ", process.env.PWD);
-console.log("process.cwd(): ", process.cwd());
-console.log("process.env.INIT_CWD: ", process.env.INIT_CWD);
-console.log("originalCwd: ", originalCwd);
-
 const child = spawn("npx", ["next", "start", "-p", "5555"], {
   stdio: "inherit",
   cwd: originalCwd,
