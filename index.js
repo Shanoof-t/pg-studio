df11f593fc4478694481166eb2f8da5661ec3623
Shanoof <shanoof.001@gmail.com>
2025-08-16 12:26:54 +0530

    fix:.env path

--- index.js
+++ index.js
@@ -4,10 +4,22 @@ const { spawn } = require("child_process");
 require("dotenv").config();
 
 console.log("DB_URL-----", process.env.DB_URL);
+
+const DB_URL =
+  process.env.DB_URL ||
+  process.env.DATABASE_URL ||
+  process.env.PG_URL ||
+  process.env.POSTGRES_URL ||
+  process.env.PG_CONNECTION ||
+  process.env.CONNECTION_STRING;
+
 const child = spawn("npx", ["next", "start", "-p", "5555"], {
   stdio: "inherit",
   cwd: __dirname,
   shell: true,
+  env: {
+    DB_URL,
+  },
 });
 
 child.on("close", (code) => {
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,40 +1,16 @@
-import path from "path";
-import dotenv from "dotenv";
 import pg from "pg";
-import fs from "fs";
 
-console.log("process.env.PWD : ", process.env.PWD);
-console.log("process.cwd(): ", process.cwd());
-const userCwd = process.env.PWD || process.cwd();
-const envPath = path.resolve(userCwd, ".env");
+const DB_URL = process.env.DB_URL;
 
-console.log("Loading .env from:", envPath);
-
-if (!fs.existsSync(envPath)) {
-  console.error(`No .env file found at ${envPath}`);
-  process.exit(1);
-}
-
-dotenv.config({ path: envPath });
-
-const dbUrl =
-  process.env.DB_URL ||
-  process.env.DATABASE_URL ||
-  process.env.PG_URL ||
-  process.env.POSTGRES_URL ||
-  process.env.PG_CONNECTION ||
-  process.env.CONNECTION_STRING;
-
-console.log("dbUrl:", dbUrl);
-
-if (!dbUrl) {
+if (!DB_URL) {
   console.error(
     "No database connection string found. Please set one of: DB_URL, DATABASE_URL, PG_URL, POSTGRES_URL, PG_CONNECTION, CONNECTION_STRING"
   );
   process.exit(1);
 }
 
-const pool = new pg.Pool({ connectionString: dbUrl });
+console.log("DB_URL in db: ", DB_URL);
+const pool = new pg.Pool({ connectionString: DB_URL });
 
 export const query = async (queryText: string, params?: string[]) => {
   try {
