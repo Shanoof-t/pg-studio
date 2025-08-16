4d99c3db0365034c299bbb1d7ceb516e0a709e76
Shanoof <shanoof.001@gmail.com>
2025-08-16 12:52:15 +0530

    final commit

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 #!/usr/bin/env node
 
 const { spawn } = require("child_process");
-// require("dotenv").config();
+require("dotenv").config();
+
+const DB_URL =
+  process.env.DB_URL ||
+  process.env.DATABASE_URL ||
+  process.env.PG_URL ||
+  process.env.POSTGRES_URL ||
+  process.env.PG_CONNECTION ||
+  process.env.CONNECTION_STRING;
 
 const child = spawn("npx", ["next", "start", "-p", "5555"], {
   stdio: "inherit",
   cwd: __dirname,
   shell: true,
+  env: {
+    ...process.env,
+    DB_URL,
+  },
 });
 
 child.on("close", (code) => {
